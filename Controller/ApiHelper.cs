d7bba8b9d1959f4264e17cb97113f43e2119e275
oscarsandberg92 <104254413+oscarsandberg92@users.noreply.github.com>
2022-12-15 16:42:50 +0100

    .

--- Controller/ApiHelper.cs
+++ Controller/ApiHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace TUCDashboardGrp1.Controller
 {
     public class ApiHelper
     {
-        // All call to API's will go through this so that unneccessary port won't be opened.
-        public static HttpClient ApiClient { get; set; } = new HttpClient();
+        // All call to API's will go through this so that unneccessary ports won't be opened. 
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient() 
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
 
     }
 }
