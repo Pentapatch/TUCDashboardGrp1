ae53e54d9d4f33e203923946dc06adf19eab433b
deliacirstea <deliutacirstea@gmail.com>
2022-12-15 18:42:35 +0100

    update

---
+++ View/ClockWidget.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TUCDashboardGrp1.View
+{
+    public partial class ClockWidget : UserControl
+    {
+        public ClockWidget()
+        {
+            InitializeComponent(); //dont touch!!!
+        }
+
+        
+
+        private void ClockWidget_Load_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+
+            label2.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
