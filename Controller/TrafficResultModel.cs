85b9d27b819118412f47ca52ff27c1590222718d
simond2991 <simon_d_32@hotmail.com>
2022-12-21 15:26:42 +0100

    Traffic API working as intended(?)

---
+++ Controller/TrafficResultModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TUCDashboardGrp1.Controller
+{
+    public class TrafficResultModel
+    {
+        public TrafficParameters[] Departure { get; set; } = Array.Empty<TrafficParameters>();
+
+    }
+
+
+    public class TrafficParameters
+    {
+        public DateTime Time { get; set; } = new();
+
+        public string Direction { get; set; } = string.Empty;
+    }
+}
