09e2aff6c631675562dd2f576980175e787a8b4e
oscarsandberg92 <104254413+oscarsandberg92@users.noreply.github.com>
2023-01-06 11:30:17 +0100

    Fixed the bug, added a setting to set opening hours, and some validation in the bookingpanel.

--- Model/Settings.cs
+++ Model/Settings.cs
@@ -1,9 +1,56 @@
-﻿namespace TUCDashboardGrp1.Model
+﻿using System.Xml.Serialization;
+
+namespace TUCDashboardGrp1.Model
 {
     public class Settings
     {
 
         public string DataPath { get; set; } = @$"{Application.UserAppDataPath}\Settings\AppData1.xml";
+
+        #region XMLIgnore
+
+        [XmlIgnore]
+        public TimeOnly? OpeningHour { get; set; }
+
+        [XmlIgnore]
+        public TimeOnly? ClosingHour { get; set; }
+        
+        #endregion
+
+        #region XMLElements
+
+        [XmlElement("XMLOpeningHour")]
+        public string XMLOpeningHour
+        {
+            get {
+                if (OpeningHour == null) return "";
+                
+                TimeOnly time = (TimeOnly)OpeningHour;
+                return time.ToShortTimeString();
+            }
+            set
+            {
+                if (value != "") OpeningHour = TimeOnly.FromDateTime(Convert.ToDateTime(value));
+            }
+        }
+
+        [XmlElement("XMLClosingHour")]
+        public string XMLClosingHour
+        {
+            get
+            {
+                if (ClosingHour == null) return "";
+
+                TimeOnly time = (TimeOnly)ClosingHour;
+                return time.ToShortTimeString();
+            }
+            set
+            {
+                if (value != "") ClosingHour = TimeOnly.FromDateTime(Convert.ToDateTime(value));
+            }
+        }
+        
+        #endregion
 
     }
 }
